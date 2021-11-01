9109ae0a5e66735857d721b269eb5cc48c7f7f82
LukaZyrgon <58442639+LukaZyrgon@users.noreply.github.com>
2021-11-01 13:19:59 +0100

    Update OBPress.php

--- OBPress.php
+++ OBPress.php
@@ -4,7 +4,7 @@
   Plugin uri: www.zyrgon.net
   Text Domain: OBPress_SearchBarPlugin
   Description: Widgets to OBPress
-  Version: 1.0.1
+  Version: 1.0.0
   Author: Zyrgon
   Author uri: www.zyrgon.net
   License: GPlv2 or Later
